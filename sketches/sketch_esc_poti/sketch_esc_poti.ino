8257830c3645657e8f8f4e4d04437a1e533e0ccc
Slx-hub <schneja@tuta.io>
2023-09-16 12:12:51 +0200

    new turntable project

--- sketches/sketch_esc_poti/sketch_esc_poti.ino
+++ sketches/sketch_esc_poti/sketch_esc_poti.ino
@@ -2,7 +2,7 @@
  
 Servo myservo;  // create servo object to control a servo 
  
-int potpin = A7;  // analog pin used to connect the potentiometer
+int potpin = A0;  // analog pin used to connect the potentiometer
 int datapin = 3; // pin to connect to the ESC
 int val;    // variable to read the value from the analog pin 
  
@@ -16,7 +16,7 @@ void setup()
 void loop() 
 { 
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023) 
-  val = map(val, 0, 1023, 40, 150);     // scale it to use it with the servo (value between 0 and 180) 
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180) 
 
   Serial.println(val);
   Serial.print(" ");
